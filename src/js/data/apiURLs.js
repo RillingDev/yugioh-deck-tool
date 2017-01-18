aab217a2063a5211a98a0f36db898ce4ce3a72e4
TheBinaryBee <coolicooli1@hotmail.de>
2017-01-18 21:40:18 +0100

    updated cardlist

--- src/js/data/apiURLs.js
+++ src/js/data/apiURLs.js
@@ -1,6 +1,6 @@
 "us strict";
 
-export const nameAPI = "./api/names/names.min.json";
-export const priceAPI = "./api/prices/prices.php?n=";
+export const nameAPI = "https://ygoprodeck.com/priceapp/api/names/names.min.json";
+export const priceAPI = "https://ygoprodeck.com/priceapp/api/prices/prices.php?n=";
 export const imageAPI = "https://ygoprodeck.com/pics";
 export const buyAPI = "http://yugiohprices.com/card_price?name=";
