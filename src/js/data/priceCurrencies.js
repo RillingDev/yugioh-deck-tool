d5cde28edcfec7038a858641ad659d1bce6786b2
FelixRilling <contact@f-rilling.com>
2017-03-13 09:06:45 +0100

    updated currency courses

--- src/js/data/priceCurrencies.js
+++ src/js/data/priceCurrencies.js
@@ -9,22 +9,22 @@ const priceCurrencies = [{
     id: "euro",
     name: "Euro",
     label: "€",
-    val: 0.941925
+    val: 0.935003
 }, {
     id: "pound",
     name: "Pound",
     label: "£",
-    val: 0.805737
+    val: 0.818227
 }, {
     id: "dollar_ca",
     name: "Canadian Dollar",
     label: "$",
-    val: 1.30955
+    val: 1.34535
 }, {
     id: "dollar_au",
     name: "Australian Dollar",
     label: "$",
-    val: 1.30480
+    val: 1.32018
 }];
 
 export default priceCurrencies;
