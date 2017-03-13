34a988be7f9c973461755e986d5df581d8875cd2
FelixRilling <contact@f-rilling.com>
2017-03-13 09:30:56 +0100

    moved coloring to css

--- src/js/data/deckParts.js
+++ src/js/data/deckParts.js
@@ -4,19 +4,16 @@ const deckParts = [{
     id: "main",
     name: "Main",
     fileId: "#main",
-    color: "#907057",
     size: [40, 60]
 }, {
     id: "extra",
     name: "Extra",
     fileId: "#extra",
-    color: "#935293",
     size: [0, 15]
 }, {
     id: "side",
     name: "Side",
     fileId: "!side",
-    color: "#54925a",
     size: [0, 15]
 }];
 
--- src/js/data/priceModes.js
+++ src/js/data/priceModes.js
@@ -3,15 +3,12 @@
 const priceModes = [{
     id: "low",
     name: "Low",
-    color: "#ddffc9"
 }, {
     id: "average",
     name: "Average",
-    color: "#fff8b7"
 }, {
     id: "high",
     name: "High",
-    color: "#ffdad0"
 }];
 
 export default priceModes;
