a793aa007e0d9f9f37df1697647b3396515ea93a
Felix Rilling <contact@f-rilling.com>
2017-11-03 14:58:40 +0100

    updated currency rates

--- app/app.js
+++ app/app.js
@@ -8490,25 +8490,25 @@ const priceCurrencies = [{
         id: "euro",
         name: "Euro",
         label: "€",
-        val: 0.848269
+        val: 0.858
     },
     {
         id: "pound",
         name: "Pound",
         label: "£",
-        val: 0.751468
+        val: 0.763
     },
     {
         id: "dollar_ca",
         name: "Canadian Dollar",
         label: "$",
-        val: 1.24888
+        val: 1.274
     },
     {
         id: "dollar_au",
         name: "Australian Dollar",
         label: "$",
-        val: 1.26938
+        val: 1.303
     }
 ];
 
