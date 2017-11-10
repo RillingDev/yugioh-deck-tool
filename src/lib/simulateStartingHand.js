faf067323e516b2573bc3d4ab4b0c0775e2fd928
Felix Rilling <contact@f-rilling.com>
2017-11-10 16:12:41 +0100

    changed shuffle algorithm

--- src/lib/simulateStartingHand.js
+++ src/lib/simulateStartingHand.js
@@ -1,12 +1,27 @@
 import {
-    arrClone
+    arrClone,
+    arrRemoveIndex
 } from "lightdash";
 
-// For some reason this is a very weird random in chrome
-const sortShuffle = () => Math.random() < 0.5;
+/**
+ * Chrome seems to have a very weird behavior when trying to shuffle the array that seems to be very un-random
+ * Therefore we use a self-built shuffle
+ */
+const fisherYatesShuffle = arr => {
+    let input = arrClone(arr);
+    const result = [];
 
-const simulateStartingHand = (cardListMain, cardsToDraw) => arrClone(cardListMain)
-    .sort(sortShuffle)
-    .slice(0, cardsToDraw);
+    while (result.length < arr.length) {
+        const index = Math.floor(Math.random() * input.length);
+        const val = input[index];
+
+        result.push(val);
+        input = arrRemoveIndex(input, index);
+    }
+
+    return result;
+};
+
+const simulateStartingHand = (cardListMain, cardsToDraw) => fisherYatesShuffle(cardListMain).slice(0, cardsToDraw);
 
 export default simulateStartingHand;
