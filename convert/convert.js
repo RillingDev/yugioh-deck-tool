edddea9f2d028a4ed2f706c19bb3cd944a88da15
Felix Rilling <felix@f-rilling.com>
2018-02-28 20:18:40 +0100

    improved converting

--- convert/convert.js
+++ convert/convert.js
@@ -1,37 +1,38 @@
 "use strict";
 
-const REGEX_IS_TOKEN = /Tokens?$/;
-
+const { isString } = require("lightdash");
 const fs = require("fs");
 const deflate = require("zlib").deflateSync;
 const input = require("./input.json");
 const inputCards = input[2].data;
 const output = {};
 
+const normalize = val => (isString(val) ? val.trim() : "");
+
 inputCards.forEach(entry => {
-    if (entry.name.length > 0 && !REGEX_IS_TOKEN.test(entry.name)) {
-        entry.linkmarkers = entry.linkmarkers
+    if (entry.name.length > 0 && !entry.type.startsWith("Token")) {
+        const linkmarkers = entry.linkmarkers
             ? entry.linkmarkers.split(",").map(str => str.trim())
             : [];
 
         output[entry.id] = [
-            entry.name,
+            normalize(entry.name),
 
-            entry.type,
-            entry.atk,
-            entry.def,
-            entry.level,
-            entry.race,
-            entry.attribute,
-            entry.linkmarkers,
+            normalize(entry.type),
+            normalize(entry.atk),
+            normalize(entry.def),
+            normalize(entry.level),
+            normalize(entry.race),
+            normalize(entry.attribute),
+            linkmarkers,
 
-            entry.times,
-            entry.rating_up,
-            entry.rating_down,
+            normalize(entry.times),
+            normalize(entry.rating_up),
+            normalize(entry.rating_down),
 
-            entry.format,
-            entry.ban_tcg,
-            entry.bab_ocg
+            normalize(entry.format),
+            normalize(entry.ban_tcg),
+            normalize(entry.bab_ocg)
         ];
     }
 });
