91bac0ef0cd997bdcac66203c6581728451f3408
Felix Rilling <felix@f-rilling.com>
2018-03-01 18:54:35 +0100

    changed format conversion

--- convert/convert.js
+++ convert/convert.js
@@ -1,20 +1,27 @@
 "use strict";
 
-const { isString } = require("lightdash");
 const fs = require("fs");
 const deflate = require("zlib").deflateSync;
 const input = require("./input.json");
+const { isString } = require("lightdash");
+
 const inputCards = input[2].data;
 const output = {};
 
 const normalize = val => (isString(val) ? val.trim() : "");
 
+const splitLinkMarkers = val =>
+    val ? val.split(",").map(str => str.trim()) : [];
+
+const banlistToNumber = val => {
+    if (val === "Banned") return 0;
+    else if (val === "Limited") return 1;
+    else if (val === "Semi-Limited") return 2;
+    return 3;
+};
+
 inputCards.forEach(entry => {
     if (entry.name.length > 0 && !entry.type.startsWith("Token")) {
-        const linkmarkers = entry.linkmarkers
-            ? entry.linkmarkers.split(",").map(str => str.trim())
-            : [];
-
         output[entry.id] = [
             normalize(entry.name),
 
@@ -24,15 +31,15 @@ inputCards.forEach(entry => {
             normalize(entry.level),
             normalize(entry.race),
             normalize(entry.attribute),
-            linkmarkers,
+            splitLinkMarkers(entry.linkmarkers),
 
             normalize(entry.times),
             normalize(entry.rating_up),
             normalize(entry.rating_down),
 
             normalize(entry.format),
-            normalize(entry.ban_tcg),
-            normalize(entry.bab_ocg)
+            banlistToNumber(entry.ban_tcg),
+            banlistToNumber(entry.bab_ocg)
         ];
     }
 });
