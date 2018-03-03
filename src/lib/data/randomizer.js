cd92badff30f05e6cecb068a515342a35e62bae1
Felix Rilling <felix@f-rilling.com>
2018-03-03 12:13:50 +0100

    disabled unfinished random modes

--- src/lib/data/randomizer.js
+++ src/lib/data/randomizer.js
@@ -1,10 +1,12 @@
 const RANDOMIZER_MODES = [
-    { name: "Fully Random", filter: card => card },
-    { name: "Single Attribute", filter: card => card },
-    { name: "Single Race", filter: card => card },
-    { name: "Single Archetype", filter: card => card },
+    { name: "Fully Random", filter: () => true }
+    /*{ name: "Single Attribute", filter: card => card },
+    { name: "Single Race", filter: card => card }
+         { name: "Single Archetype", filter: card => card },
     { name: "Dual Archetypes", filter: card => card },
-    { name: "Triple Archetypes", filter: card => card }
+    { name: "Triple Archetypes", filter: card => card } */
 ];
+
+Object.freeze(RANDOMIZER_MODES);
 
 export { RANDOMIZER_MODES };
