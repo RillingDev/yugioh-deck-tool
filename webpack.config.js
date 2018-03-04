57f3d278d686fb50495f9f899a2731624608f84b
Felix Rilling <felix@f-rilling.com>
2018-03-04 14:01:27 +0100

    updated stack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const PRODUCTION = process.env.NODE_ENV === "production";
+const NODE_ENV = process.env.NODE_ENV;
+const PRODUCTION_ENABLED = NODE_ENV === "production";
 const CACHE = "./.cache/";
 
 const webpack = require("webpack");
@@ -11,10 +12,11 @@ const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
  */
 const pluginExtractText = new ExtractTextPlugin("app.css");
 const pluginEnv = new webpack.DefinePlugin({
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
 });
 const pluginUglify = new UglifyJsPlugin({
-    cache: path.join(CACHE, "uglifyjs/")
+    cache: path.join(CACHE, "uglifyjs/"),
+    sourceMap: true
 });
 
 /**
@@ -43,6 +45,13 @@ const ruleBabel = {
         }
     }
 };
+const ruleExtractText = {
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: "css-loader"
+    })
+};
 
 module.exports = {
     entry: "./src/index.js",
@@ -51,11 +60,13 @@ module.exports = {
         publicPath: "/dist/",
         filename: "app.js"
     },
-    plugins: PRODUCTION
-        ? [pluginExtractText, pluginEnv, pluginUglify]
-        : [pluginExtractText, pluginEnv],
+    mode: NODE_ENV,
+    plugins: PRODUCTION_ENABLED
+        ? [pluginEnv, pluginUglify, pluginExtractText]
+        : [pluginEnv, pluginExtractText],
     module: {
-        rules: PRODUCTION ? [ruleVue, ruleEslint, ruleBabel] : [ruleVue]
-    },
-    devtool: PRODUCTION ? "" : "eval-source-map"
+        rules: PRODUCTION_ENABLED
+            ? [ruleVue, ruleExtractText, ruleEslint, ruleBabel]
+            : [ruleVue, ruleExtractText]
+    }
 };
