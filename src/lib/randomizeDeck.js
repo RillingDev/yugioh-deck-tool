fa1a3ee5abaf9c665032f6625c22ae5cf20acc14
Felix Rilling <felix@f-rilling.com>
2018-03-10 23:51:18 +0100

    started randomizer rework

--- src/lib/randomizeDeck.js
+++ src/lib/randomizeDeck.js
@@ -1,5 +1,6 @@
 import Deck from "./classes/deck";
 import shuffle from "./shuffle";
+import { DECKPARTS } from "./data/deck";
 
 const REGEX_NAME_DELIMITER = /\s?[,;:\- ]?\s/;
 
@@ -36,15 +37,104 @@ const getRandomName = cardNameList => {
 };
 
 const randomizeDeck = (cardDb, filter, deckParts) => {
-    const pairsShuffled = shuffle(cardDb.pairsArr.filter(filter));
-    const result = [];
+    const addCardRandomAmount = (arr, card, limit, preferPlayset = true) => {
+        const cardAmountMaxFromBanlist = card[1][12];
+        let cardAmountRandom = getRandomAmount(preferPlayset);
+
+        if (cardAmountRandom > cardAmountMaxFromBanlist) {
+            cardAmountRandom = cardAmountMaxFromBanlist;
+        }
+        if (arr.length + cardAmountRandom > limit) {
+            cardAmountRandom = limit - arr.length;
+        }
+
+        if (cardAmountRandom > 0) {
+            resultCardNames.push(card[1][1]);
+
+            return [...arr, ...new Array(cardAmountRandom).fill(card[0])];
+        }
+
+        return arr;
+    };
+    const pairsShuffled = shuffle(cardDb.pairsArr.slice(0, 100)); //shuffle(cardDb.pairsArr.filter(filter));
+    const result = [[], [], []];
     const resultCardNames = [];
     let i = 0;
+    let mainDeckCountSpells = 0;
+    let mainDeckCountTraps = 0;
 
     // eslint-disable-next-line no-console
     console.log(pairsShuffled);
 
-    deckParts.forEach(deckPart => {
+    while (
+        (result[0].length < deckParts[0].min ||
+            result[1].length < deckParts[1].max ||
+            result[2].length < deckParts[2].max) &&
+        i < pairsShuffled.length
+    ) {
+        const card = pairsShuffled[i];
+
+        if (
+            deckParts[0].check(card[1]) &&
+            result[0].length < deckParts[0].min
+        ) {
+            /**
+             * Main
+             */
+            const isSpell = card[1][1] === "Spell Card";
+            const isTrap = card[1][1] === "Trap Card";
+
+            console.log({ mainDeckCountSpells, mainDeckCountTraps });
+
+            if (
+                (!isSpell || mainDeckCountSpells < MAX_SPELLS) &&
+                (!isTrap || mainDeckCountTraps < MAX_TRAPS)
+            ) {
+                const prevLength = result[0].length;
+
+                result[0] = addCardRandomAmount(
+                    result[0],
+                    card,
+                    deckParts[0].min
+                );
+
+                if (isSpell) {
+                    mainDeckCountSpells += result[0].length - prevLength;
+                } else if (isTrap) {
+                    mainDeckCountTraps += result[0].length - prevLength;
+                }
+            }
+        } else if (
+            deckParts[1].check(card[1]) &&
+            result[1].length < deckParts[1].max
+        ) {
+            /**
+             * Extra
+             */
+            result[1] = addCardRandomAmount(
+                result[1],
+                card,
+                deckParts[1].max,
+                false
+            );
+        } else if (
+            deckParts[2].check(card[1]) &&
+            result[2].length < deckParts[2].max
+        ) {
+            /**
+             * Side
+             */
+            result[2] = addCardRandomAmount(
+                result[2],
+                card,
+                deckParts[2].max,
+                false
+            );
+        }
+
+        i++;
+    }
+    /*  deckParts.forEach(deckPart => {
         const subResult = [];
         const deckPartLimit = deckPart.min === 0 ? deckPart.max : deckPart.min;
         const isExtra = deckPart.id === "extra";
@@ -52,32 +142,34 @@ const randomizeDeck = (cardDb, filter, deckParts) => {
         let countSpells = 0;
         let countTraps = 0;
 
-        while (subResult.length < deckPartLimit && i < pairsShuffled.length) {
+       while (subResult.length < deckPartLimit && i < pairsShuffled.length) {
             const card = pairsShuffled[i];
             const isSpell = card[1][1] === "Spell Card";
             const isTrap = card[1][1] === "Trap Card";
 
             if (
-                deckPart.check(card[1]) &&
                 (!isSpell || countSpells < MAX_SPELLS) &&
                 (!isTrap || countTraps < MAX_TRAPS)
             ) {
-                const cardAmount = getRandomAmount(!(isExtra || isSide));
-                const cardAmountMax = card[1][12];
-                const cardByAmount = new Array(
-                    cardAmountMax >= cardAmount ? cardAmount : cardAmountMax
-                ).fill(card[0]);
+                if (deckPart.check(card[1])) {
+                    const cardAmount = getRandomAmount(!(isExtra || isSide));
+                    const cardAmountMax = card[1][12];
+                    const cardByAmount = new Array(
+                        cardAmountMax >= cardAmount ? cardAmount : cardAmountMax
+                    ).fill(card[0]);
 
-                subResult.push(...cardByAmount);
+                    subResult.push(...cardByAmount);
 
-                if (isSpell) {
-                    countSpells += cardAmount;
-                } else if (isTrap) {
-                    countTraps += cardAmount;
-                }
+                    if (isSpell) {
+                        countSpells += cardAmount;
+                    } else if (isTrap) {
+                        countTraps += cardAmount;
+                    }
 
-                if (!isSide && cardAmount === 3) {
-                    resultCardNames.push(card[1][0]);
+                    if (!isSide && cardAmount === 3) {
+                        resultCardNames.push(card[1][0]);
+                    }
+                } else {
                 }
             }
 
@@ -85,7 +177,9 @@ const randomizeDeck = (cardDb, filter, deckParts) => {
         }
 
         result.push(subResult.slice(0, deckPartLimit));
-    });
+    }); */
+
+    console.log(result);
 
     return new Deck(result, getRandomName(resultCardNames)).sort(cardDb);
 };
