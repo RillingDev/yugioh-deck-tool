4767d346845cad9f0108f183a427a262c9141707
Felix Rilling <felix@f-rilling.com>
2018-03-10 08:09:09 +0100

    refactored search patterns

--- src/lib/searchCard.js
+++ src/lib/searchCard.js
@@ -4,29 +4,46 @@ const optionFilter = (val, filterItem) =>
 const optionFilterArr = (val, filterItem) =>
     filterItem.active === "Any" ? true : val.includes(filterItem.active);
 
-const searchCard = (cardArr, filter, is, sortFn) =>
-    cardArr
-        // Filter Text and other filters
-        .filter(
-            pair =>
-                // Search Name
-                pair[1][0].toLowerCase().includes(filter.name.toLowerCase()) &&
-                // Search Type
-                optionFilter(pair[1][1], filter.type) &&
-                // Search Monster Sub
-                (!is.monster || optionFilter(pair[1][6], filter.attribute)) &&
-                (!is.monster || optionFilter(pair[1][5], filter.race)) &&
-                (!is.monster || optionFilter(pair[1][4], filter.level)) &&
-                (!is.monsterLink ||
-                    optionFilterArr(pair[1][7], filter.linkarrows)) &&
-                // Search Spell Sub
-                (!is.spell || optionFilter(pair[1][5], filter.spelltype)) &&
-                // Search Trap Sub
-                (!is.trap || optionFilter(pair[1][5], filter.traptype))
-        )
-        // Apply sorting
-        .sort((a, b) => sortFn(a[1], b[1]))
-        // Drop everything but id and name
-        .map(pair => [pair[0], pair[1][0]]);
+const searchCard = (cardArr, filter, is, sortFn) => {
+    const filterNameLower = filter.name.toLowerCase();
+
+    return (
+        cardArr
+            // Filter Text and other filters
+            .filter(pair => {
+                const optionFilterPattern = (
+                    cond,
+                    index,
+                    filterVal,
+                    optionFn = optionFilter
+                ) => !cond || optionFn(pair[1][index], filterVal);
+
+                return (
+                    // Search Name
+                    pair[1][0].toLowerCase().includes(filterNameLower) &&
+                    // Search Type
+                    optionFilter(pair[1][1], filter.type) &&
+                    // Search Monster Sub
+                    optionFilterPattern(is.monster, 6, filter.attribute) &&
+                    optionFilterPattern(is.monster, 5, filter.race) &&
+                    optionFilterPattern(is.monster, 4, filter.level) &&
+                    optionFilterPattern(
+                        is.monsterLink,
+                        7,
+                        filter.linkarrows,
+                        optionFilterArr
+                    ) &&
+                    // Search Spell Sub
+                    optionFilterPattern(is.spell, 5, filter.spelltype) &&
+                    // Search Trap Sub
+                    optionFilterPattern(is.trap, 5, filter.traptype)
+                );
+            })
+            // Apply sorting
+            .sort((a, b) => sortFn(a[1], b[1]))
+            // Drop everything but id and name
+            .map(pair => [pair[0], pair[1][0]])
+    );
+};
 
 export default searchCard;
