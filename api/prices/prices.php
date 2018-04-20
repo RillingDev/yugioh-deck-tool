4c2db9e6994e9e0ef29c4d90e65055fb6f32b1a0
Felix Rilling <felix@f-rilling.com>
2018-04-20 16:47:14 +0200

    improved price fetching

--- api/prices/prices.php
+++ api/prices/prices.php
@@ -5,6 +5,8 @@ $cardNames = json_decode(base64_decode($cardUri));
 $requests = [];
 $result = [];
 
+
+
 // Creates a curl object for every name to query
 foreach ($cardNames as $currentIndex => $cardName) {
     $url = "http://yugiohprices.com/api/get_card_prices/".urlencode($cardName);
@@ -14,14 +16,12 @@ foreach ($cardNames as $currentIndex => $cardName) {
     curl_setopt($requests[$currentIndex], CURLOPT_RETURNTRANSFER, 1);
 }
 
-// Build the multi-curl handle, adding all curls
 $mh = curl_multi_init();
 
 foreach ($requests as $request) {
     curl_multi_add_handle($mh, $request);
 }
 
-// Execute all queries simultaneously, and continue when all are complete
 $running = null;
 
 do {
@@ -34,6 +34,8 @@ foreach ($requests as $request) {
 }
 
 curl_multi_close($mh);
+
+
 
 // Transforms output
 foreach ($requests as $request) {
