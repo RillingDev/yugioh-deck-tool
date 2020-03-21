4d1761646ff5482fd95451ffe53e3239e909f799
FelixRilling <contact@rilling.dev>
2020-03-21 14:14:11 +0100

    Limit skill cards to 1.

--- core/src/core/business/service/DeckService.ts
+++ core/src/core/business/service/DeckService.ts
@@ -8,6 +8,7 @@ import { Format } from "../../model/ygo/Format";
 import { removeItem } from "lightdash";
 import { clone } from "lodash";
 import { SortingService, SortingStrategy } from "./SortingService";
+import { CardTypeGroup } from "../../model/ygo/CardTypeGroup";
 
 @injectable()
 class DeckService {
@@ -28,15 +29,29 @@ class DeckService {
         format: Format | null,
         card: Card
     ): boolean {
+        // If the card is not allowed in this deckpart, return false
         if (!card.type.deckPart.has(deckPart)) {
             return false;
         }
 
+        // If adding would make the deckpart be larger than allowed, return false
         const deckPartSize = deck.parts.get(deckPart)!.length;
         if (deckPartSize + 1 > deckPart.max) {
             return false;
         }
 
+        // If a skill card would be added with one already existing, return false
+        if (
+            card.type.group === CardTypeGroup.SKILL &&
+            this.getAllCards(deck).some(
+                existingCard => existingCard.type.group === CardTypeGroup.SKILL
+            )
+        ) {
+            return false;
+        }
+
+        // If adding this card would make the total count of this card in this deck
+        // be larger than allowed by the banlist, return false
         const count = this.getAllCards(deck).filter(existingCard =>
             this.cardService.isTreatedAsSame(existingCard, card)
         ).length;
@@ -66,7 +81,7 @@ class DeckService {
                 deckPart,
                 this.sortingService.sort(
                     deckClone.parts.get(deckPart)!,
-                    SortingStrategy.DECK,
+                    SortingStrategy.DECK
                 )
             );
         }
@@ -78,9 +93,7 @@ class DeckService {
         for (const deckPart of DEFAULT_DECK_PART_ARR) {
             deckClone.parts.set(
                 deckPart,
-                this.sortingService.shuffle(
-                    deckClone.parts.get(deckPart)!
-                )
+                this.sortingService.shuffle(deckClone.parts.get(deckPart)!)
             );
         }
         return deckClone;
