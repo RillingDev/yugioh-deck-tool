9618cb56c529de6d8678a27e0bf4b521c51b18ae
FelixRilling <contact@rilling.dev>
2020-03-21 13:56:40 +0100

    Re-added name randomization.

--- core/src/core/business/service/DeckRandomizationService.ts
+++ core/src/core/business/service/DeckRandomizationService.ts
@@ -4,23 +4,29 @@ import { Deck } from "../../model/ygo/Deck";
 import { CardDatabase } from "../CardDatabase";
 import { DeckService } from "./DeckService";
 import { TYPES } from "../../../types";
-import { DeckPart, DEFAULT_DECK_PART_ARR } from "../../model/ygo/DeckPart";
+import {
+    DeckPart,
+    DEFAULT_DECK_PART_ARR,
+    DefaultDeckPart
+} from "../../model/ygo/DeckPart";
 import { SortingService } from "./SortingService";
 import { CardService } from "./CardService";
-import { min } from "lodash";
+import { min, random, shuffle, words } from "lodash";
 import { Card } from "../../model/ygo/Card";
 import { Format } from "../../model/ygo/Format";
 
 enum RandomizationStrategy {
-    NORMAL,
-    ARCHETYPE_1,
-    ARCHETYPE_2,
-    ARCHETYPE_3,
-    HIGHLANDER
+    NORMAL = "Normal",
+    ARCHETYPE_1 = "1 Archetype",
+    ARCHETYPE_2 = "2 Archetypes",
+    ARCHETYPE_3 = "3 Archetypes",
+    HIGHLANDER = "Highlander"
 }
 
 @injectable()
 class DeckRandomizationService {
+    private static readonly IGNORED_WORDS = ["of", "the", "a", "an", "in"];
+
     private readonly cardDatabase: CardDatabase;
     private readonly deckService: DeckService;
     private readonly filterService: FilterService;
@@ -79,7 +85,7 @@ class DeckRandomizationService {
                 i++;
             }
         }
-
+        deck.name = this.createName(deck);
         return this.deckService.sort(deck);
     }
 
@@ -119,6 +125,31 @@ class DeckRandomizationService {
             return 2;
         }
         return 3;
+    }
+
+    private createName(deck: Deck): string {
+        const cards = [
+            ...deck.parts.get(DefaultDeckPart.MAIN)!,
+            ...deck.parts.get(DefaultDeckPart.EXTRA)!
+        ];
+        const wordCount = random(2, 3, false);
+        return this.sortingService
+            .shuffle(cards)
+            .slice(0, wordCount)
+            .map(card => this.getRandomWord(card))
+            .join(" ");
+    }
+
+    private getRandomWord(card: Card): string {
+        const shuffledWords = shuffle(
+            words(card.name).filter(
+                word => !DeckRandomizationService.IGNORED_WORDS.includes(word)
+            )
+        );
+        if (shuffledWords.length === 0) {
+            throw new Error("No words lefter after filtering!");
+        }
+        return shuffledWords[0];
     }
 }
 
