4d62af22136328887130eb03baff37cb3c1e8cfe
FelixRilling <contact@rilling.dev>
2020-04-19 19:45:19 +0200

    Remove currency locale.

--- application/src/lib/controller/PriceController.ts
+++ application/src/lib/controller/PriceController.ts
@@ -3,11 +3,11 @@ import {
     Card,
     Currency,
     DEFAULT_CURRENCY_ARR,
-    DefaultVendor,
+    DEFAULT_VENDOR_ARR,
+    DefaultCurrency,
     PriceService,
     TYPES,
     Vendor,
-    DEFAULT_VENDOR_ARR,
 } from "../../../../core/src/main";
 import { createCurrencyFormatter } from "../../../../ui/src/main";
 
@@ -22,7 +22,7 @@ class PriceController {
         this.priceService = priceService;
         this.vendors = DEFAULT_VENDOR_ARR;
         this.currencies = DEFAULT_CURRENCY_ARR;
-        this.activeCurrency = this.guessDefaultCurrency();
+        this.activeCurrency = DefaultCurrency.USD;
     }
 
     public getPriceByVendor(cards: Card[]): Map<Vendor, number> {
@@ -40,16 +40,6 @@ class PriceController {
 
     public format(val: number): string {
         return createCurrencyFormatter(this.activeCurrency).format(val);
-    }
-
-    private guessDefaultCurrency() {
-        const localeIndex = DEFAULT_CURRENCY_ARR.findIndex(
-            (currency) => currency.locale === navigator.language
-        );
-
-        return localeIndex === -1
-            ? DEFAULT_CURRENCY_ARR[0]
-            : DEFAULT_CURRENCY_ARR[localeIndex];
     }
 }
 
--- core/src/core/model/price/Currency.ts
+++ core/src/core/model/price/Currency.ts
@@ -2,7 +2,6 @@ import { deepFreeze } from "lightdash";
 
 interface Currency {
     readonly id: string;
-    readonly locale: string;
     readonly name: string;
     readonly conversionRate: number; // Value of 1 USD in this currency
     readonly fractionDigits: number;
@@ -22,63 +21,54 @@ const DefaultCurrency: {
 } = {
     USD: {
         id: "USD",
-        locale: "en-US",
         name: "US Dollar",
         conversionRate: 1.0,
         fractionDigits: 2,
     },
     EUR: {
         id: "EUR",
-        locale: "de-DE",
         name: "Euro",
         conversionRate: 0.9272137228,
         fractionDigits: 2,
     },
     GBP: {
         id: "GBP",
-        locale: "en-GB",
         name: "British Pound",
         conversionRate: 0.8145572554,
         fractionDigits: 2,
     },
     CAD: {
         id: "CAD",
-        locale: "en-CA",
         name: "Canadian Dollar",
         conversionRate: 1.4185442745,
         fractionDigits: 2,
     },
     AUD: {
         id: "AUD",
-        locale: "en-AU",
         name: "Australian Dollar",
         conversionRate: 1.6693555865,
         fractionDigits: 2,
     },
     MXN: {
         id: "MXN",
-        locale: "es-MX",
         name: "Mexican Peso",
         conversionRate: 24.6147426982,
         fractionDigits: 1,
     },
     BRL: {
         id: "BRL",
-        locale: "pt-BR",
         name: "Brazilian Real",
         conversionRate: 5.2751970329,
         fractionDigits: 1,
     },
     THB: {
         id: "THB",
-        locale: "th-TH",
         name: "Thai Baht",
         conversionRate: 33.0097357441,
         fractionDigits: 0,
     },
     IDR: {
         id: "IDR",
-        locale: "id-ID",
         name: "Indonesian Rupiah",
         conversionRate: 16614.4459898006,
         fractionDigits: 0,
