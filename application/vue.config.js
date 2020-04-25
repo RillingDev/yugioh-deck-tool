b6c72e6ab8a9931e59beaded9e531e891eb8c7f8
FelixRilling <contact@rilling.dev>
2020-04-25 16:57:20 +0200

    Improved vue webpack override docs.

--- application/vue.config.js
+++ application/vue.config.js
@@ -2,7 +2,10 @@ module.exports = {
     publicPath: "./",
     filenameHashing: false, // Cannot be used due to external embedding of dist output.
     chainWebpack: (config) => {
+        // We have an additional entry point for standalone tooltip usage.
         config.entry("tooltip").add("../tooltip/src/main.ts")
+
+        // Only use code common to both entry points for chunks, no vendor chunks.
         config.optimization.splitChunks({
             cacheGroups: {
                 common: {
@@ -18,9 +21,8 @@ module.exports = {
         // Unbind unused plugins/rules
         config.plugins.delete("preload");
         config.plugins.delete("prefetch");
-        config.module.rules.delete("eslint");
+        config.module.rules.delete("tsx");
         config.module.rules.delete("pug");
-        config.module.rules.delete("postcss");
         config.module.rules.delete("sass");
         config.module.rules.delete("less");
         config.module.rules.delete("stylus");
