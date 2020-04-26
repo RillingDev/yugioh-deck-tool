cc7c640ea8b38ce1aa3f51c4c90a332e616c8635
FelixRilling <contact@rilling.dev>
2020-04-26 13:21:50 +0200

    Removed unused multi-select style.

--- application/src/App.vue
+++ application/src/App.vue
@@ -301,6 +301,5 @@ export default class App extends Vue {
 
 @import "styles/blocks/general";
 @import "styles/blocks/forms";
-@import "styles/blocks/multiselect";
 @import "styles/blocks/ygoprodeck";
 </style>
