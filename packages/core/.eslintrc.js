222d0c58169eb0c3db938184c8838dc8eab5d0d0
FelixRilling <contact@rilling.dev>
2020-04-29 10:05:28 +0200

    Added lerna.

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./src/tsconfig.json", "./spec/src/tsconfig.json"]
+    },
+    plugins: ["import", "@typescript-eslint", "prettier"],
+    extends: [
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "../../.eslintrc.js",
+    ],
+    env: {
+        "jasmine": true,
+    }
+};
