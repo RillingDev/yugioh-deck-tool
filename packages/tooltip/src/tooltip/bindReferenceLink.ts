59a666854deb686c2feea59448fa749ccc7e4165
FelixRilling <contact@rilling.dev>
2020-05-05 18:56:00 +0200

    Fixed card-db handling of empty href links.

--- packages/tooltip/src/tooltip/bindReferenceLink.ts
+++ packages/tooltip/src/tooltip/bindReferenceLink.ts
@@ -8,13 +8,19 @@ const cardService = tooltipContainer.get<CardService>(
 
 const browserSupportsTouch = (): boolean => navigator.maxTouchPoints > 0;
 
+const isHrefBlank = (target: HTMLAnchorElement): boolean => {
+    // We use this over HTMLAnchorElement.prototype.href as that returns the current URL if the value is empty.
+    const href = target.attributes.getNamedItem("href")?.value;
+    return href == null || href === "";
+};
+
 export const bindReferenceLink = (
     target: HTMLAnchorElement,
     card: Card
 ): void => {
     if (
         !browserSupportsTouch() && // On touch devices binding a link causes issues with tooltips.
-        target.href.length === 0 // If a link is set already, do not bind one.
+        isHrefBlank(target) // If a link is set already, do not bind one.
     ) {
         target.href = cardService.getReferenceLink(card);
         target.target = "_blank";
