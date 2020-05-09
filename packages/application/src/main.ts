0bfa7823e3760a0d779f99440d94737c41d7009d
FelixRilling <contact@rilling.dev>
2020-05-09 19:16:35 +0200

    Added vuex.

--- packages/application/src/main.ts
+++ packages/application/src/main.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata";
 import Vue from "vue";
 import App from "./App.vue";
+import store from "./store/store";
 
 new Vue({
+    store,
     render: (h) => h(App),
 }).$mount("#decktoolApp");
