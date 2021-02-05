220b1622f91efef78d8374fc082ff97f21e1155b
FelixRilling <contact@rilling.dev>
2021-02-05 21:02:40 +0100

    Retrieve ygoprodeck credentials from current context.

--- packages/application/src/controller/YgoprodeckController.ts
+++ packages/application/src/controller/YgoprodeckController.ts
@@ -2,6 +2,13 @@ import type { Credentials } from "../../../core/src/main";
 import { Environment, EnvironmentConfig, TYPES } from "../../../core/src/main";
 import { inject, injectable } from "inversify";
 
+declare global {
+    interface Window {
+        ygoprodeckUsername?: string;
+        ygoprodeckToken?: string;
+    }
+}
+
 @injectable()
 export class YgoprodeckController {
     private readonly environmentConfig: EnvironmentConfig;
@@ -13,13 +20,27 @@ export class YgoprodeckController {
         this.environmentConfig = environmentConfig;
     }
 
+    public hasCredentials(): boolean {
+        this.validateEnv();
+        return (
+            window.ygoprodeckUsername != null && window.ygoprodeckToken != null
+        );
+    }
+
     public getCredentials(): Credentials {
+        this.validateEnv();
+        if (!this.hasCredentials()) {
+            throw new TypeError("Insufficient credentials available.");
+        }
+        return {
+            username: window.ygoprodeckUsername!,
+            token: window.ygoprodeckToken!,
+        };
+    }
+
+    private validateEnv(): void {
         if (this.environmentConfig.getEnvironment() != Environment.YGOPRODECK) {
             throw new Error("Only available in YGOPRODECK environment.");
         }
-        return {
-            username: "foo",
-            token: "bar",
-        };
     }
 }
