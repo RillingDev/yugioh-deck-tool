d82547907e0cd9c6bcf603c5d88fa6e0993902c4
FelixRilling <contact@rilling.dev>
2022-06-12 09:46:39 +0200

    Adapted ID prefix.

--- src/application/composition/id.ts
+++ src/application/composition/id.ts
@@ -4,4 +4,4 @@ let i = 0;
  * Very primitive globally unique ID generator.
  */
 // We could use UUIDs, but due to being single-threaded an incrementing number should be enough.
-export const useId = (): string => `id_${++i}`;
+export const useId = (): string => `v-${++i}`;
