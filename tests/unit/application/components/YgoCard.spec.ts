64fd15389cd1a4b09d4c97232d17a82c3d61757e
FelixRilling <contact@rilling.dev>
2022-10-29 14:36:10 +0200

    Updated test.

--- tests/unit/application/components/YgoCard.spec.ts
+++ tests/unit/application/components/YgoCard.spec.ts
@@ -1,18 +1,16 @@
 import "reflect-metadata";
 
-import { createLocalVue, shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import YgoCard from "../../../../src/application/components/YgoCard.vue";
 import type { Card } from "@/core/lib";
 import { createCard } from "../../helper/dataFactories";
+import type { ComponentOptions } from "vue";
 
 describe("YgoCard.vue", () => {
-	// TODO
 	it("binds card name", () => {
 		const card: Card = createCard({ passcode: "123", name: "foo" });
 
-		const localVue = createLocalVue();
-		const wrapper = shallowMount(YgoCard, {
-			localVue,
+		const wrapper = mount(YgoCard as ComponentOptions<Vue>, {
 			propsData: { card },
 		});
 
