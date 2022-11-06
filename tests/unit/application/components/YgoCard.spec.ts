7b2fa474f1f47c612e8a7382810f9c47c513b3ab
FelixRilling <contact@rilling.dev>
2022-11-06 18:56:01 +0100

    Fixed failing tests.

--- tests/unit/application/components/YgoCard.spec.ts
+++ tests/unit/application/components/YgoCard.spec.ts
@@ -3,6 +3,17 @@ import YgoCard from "../../../../src/application/components/YgoCard.vue";
 import type { Card } from "@/core/lib";
 import { createCard } from "../../helper/dataFactories";
 import type { ComponentOptions } from "vue";
+import type { ResourceService } from "@/ygoprodeck/api/ResourceService";
+
+jest.mock("@/application/container", () => {
+	return {
+		resourceService: {
+			getPlaceholderCardImageUrl() {
+				return "placeholderCardImageUrl";
+			},
+		} as ResourceService,
+	};
+});
 
 describe("YgoCard.vue", () => {
 	it("binds card name", () => {
