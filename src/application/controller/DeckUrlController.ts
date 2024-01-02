43f1e301cab57c3bfad9f499b548ff935744ce2b
FelixRilling <contact@rilling.dev>
2024-01-02 21:53:11 +0100

    Reapply "Removed deprecated loading of remote deck file via URL."
    This reverts commit 13bea8b572b29dd007f7dc9eb9397a092b9a6e85.

--- src/application/controller/DeckUrlController.ts
+++ src/application/controller/DeckUrlController.ts
@@ -16,10 +16,6 @@ export class DeckUrlController {
 	 * @deprecated
 	 */
 	static readonly #PARAM_LEGACY_ENCODED_URI_DECK = "e";
-	/**
-	 * @deprecated
-	 */
-	static readonly #PARAM_REMOTE_DECK = "u";
 
 	readonly #deckService: DeckService;
 	readonly #deckUriEncodingService: DeckUriEncodingService;
@@ -42,23 +38,6 @@ export class DeckUrlController {
 	 * @return Parsed deck or null if none is found.
 	 */
 	async loadUriDeck(url: URL): Promise<Deck | null> {
-		// Load deck file from a remote URL
-		const remoteUrlValue = url.searchParams.get(
-			DeckUrlController.#PARAM_REMOTE_DECK
-		);
-		if (remoteUrlValue != null) {
-			const importResult = await this.#deckFileService.fromRemoteFile(
-				new URL(location.toString()),
-				new URL(remoteUrlValue)
-			);
-			if (importResult.missing.length > 0) {
-				DeckUrlController.#logger.warn(
-					`Could not read ${importResult.missing.length} cards in remote deck.`
-				);
-			}
-			return importResult.deck;
-		}
-
 		// Load encoded URI deck
 		const uriEncodedDeck = url.searchParams.get(
 			DeckUrlController.#PARAM_ENCODED_URI_DECK
