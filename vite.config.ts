454b23192b26790a4003b736e20731537b747bd7
RillingDev <contact@rilling.dev>
2024-12-15 11:39:48 +0100

    Updated root.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
-import vue2 from "@vitejs/plugin-vue2";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue2()],
+	plugins: [vue(), vueDevTools()],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
