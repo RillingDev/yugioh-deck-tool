13f3f28f13d09472f3d37b9c6e80ca5a2005ccb0
RillingDev <contact@rilling.dev>
2024-12-24 11:31:55 +0100

    Improved asConstant

--- src/core/util.ts
+++ src/core/util.ts
@@ -1,6 +1,8 @@
+import { markRaw } from "vue";
+
 /**
  * constant objects in need to be frozen, as otherwise Vue deep proxies get injected which break business logic.
  */
-export function asConstant<T>(value: T): Readonly<T> {
-	return Object.freeze(value);
+export function asConstant<T extends object>(value: T): Readonly<T> {
+	return markRaw(value);
 }
